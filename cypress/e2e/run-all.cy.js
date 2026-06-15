69b5958f941e759cf2ee42b1ef41256e7db0ca98
tanthaina <tanthaina@gmail.com>
2026-06-15 16:39:05 +0700

    test: remove run-all.cy.js to prevent duplicate test runs in CI

--- cypress/e2e/run-all.cy.js
+++
@@ -1,6 +0,0 @@
-// ไฟล์ศูนย์รวมสำหรับสั่งรันเทสทั้งหมด (Run All Specs)
-import './01-core.cy.js'
-import './02-players.cy.js'
-import './03-smart-board.cy.js'
-import './04-accounting.cy.js'
-import './05-settings.cy.js'
